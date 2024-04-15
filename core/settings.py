5795800e1a2fd6dd9bb0330e19cb0d84bbff5b28
Edivaldo Gomes <edslavianov@gmail.com>
2024-04-15 02:14:15 +0200

    Modificación de la estructura de carpeta para el proyecto Ecommerce

--- core/settings.py
+++ core/settings.py
@@ -12,27 +12,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import environ
 from datetime import timedelta
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DOMAIN=os.environ.get('DOMAIN')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "nfjsfnjwnfnwfjnowknfoermnfkmmwekkmlñw5546544545ffw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS_DEV')
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -112,10 +108,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
+        "NAME": "Latintech_dolphin",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "db",
+        "HOST": "localhost",
         "PORT": "5432"
     }
 }
@@ -169,10 +165,10 @@ MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+"""
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
-]
+]"""
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -234,19 +230,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
-ACTIVE_CAMPAIGN_KEY=os.environ.get('ACTIVE_CAMPAIGN_KEY')
-ACTIVE_CAMPAIGN_URL=os.environ.get('ACTIVE_CAMPAIGN_URL')
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-if not DEBUG:
-    ALLOWED_HOSTS=env.list('ALLOWED_HOSTS_DEPLOY',  default=['localhost'])
-    CORS_ORIGIN_WHITELIST = env.list('CORS_ORIGIN_WHITELIST_DEPLOY',  default=['localhost'])
-    CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS_DEPLOY' , default=['localhost'])
-
-
-    DATABASES = {
-        "default": env.db("DATABASE_URL",  default=""),
-    }
-    DATABASES["default"]["ATOMIC_REQUESTS"] = True
